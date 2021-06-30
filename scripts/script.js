cc3b5a67b2d634908066aff9f9e83e786f6e0f9a
Dmitry Vabischevich <Dmitryvabischevich@gmail.com>
2021-06-30 18:31:17 +0300

    feat: добавить ввод клавиатурой и сделать адаптив

--- scripts/script.js
+++ scripts/script.js
@@ -18,6 +18,8 @@ const Button6 = form.querySelector('.form__button_number_6');
 const Button7 = form.querySelector('.form__button_number_7');
 const Button8 = form.querySelector('.form__button_number_8');
 const Button9 = form.querySelector('.form__button_number_9');
+const Button0 = form.querySelector('.form__button_number_0');
+const ButtonPoint = form.querySelector('.form__button_type_point');
 // поля:
 const inputField = form.querySelector('.form__input');
 const outputField = form.querySelector('.form__output');
@@ -74,6 +76,16 @@ function addInteger9() {
   inputField.value = inputField.value + '9';
 }
 
+function addInteger0() {
+  inputField.value = inputField.value + '0';
+}
+
+function addPoint() {
+  inputField.value = inputField.value + '.';
+}
+
+
+
 function result() {
   if (inputField.value !== '') {
     outputField.value = eval(inputField.value);
@@ -93,6 +105,71 @@ function clean() {
   outputField.value = '';
 }
 
+function PressKey(evt) {
+  switch (evt.key) {
+    case '1':
+      inputField.value = inputField.value + '1';
+        break;
+    case '2':
+      inputField.value = inputField.value + '2';
+        break;
+    case '3':
+      inputField.value = inputField.value + '3';
+        break;
+    case '4':
+      inputField.value = inputField.value + '4';
+        break;
+    case '5':
+      inputField.value = inputField.value + '5';
+        break;
+    case '6':
+      inputField.value = inputField.value + '6';
+        break;
+    case '7':
+      inputField.value = inputField.value + '7';
+        break;
+    case '8':
+      inputField.value = inputField.value + '8';
+        break;
+    case '9':
+      inputField.value = inputField.value + '9';
+        break;
+    case '0':
+      inputField.value = inputField.value + '0';
+        break;
+    case '.':
+      inputField.value = inputField.value + '.';
+        break;
+    case '+':
+      inputField.value = inputField.value + ' + ';
+        break;
+    case '-':
+      inputField.value = inputField.value + ' - ';
+        break;
+    case '*':
+      inputField.value = inputField.value + ' * ';
+        break;
+    case '/':
+      inputField.value = inputField.value + ' / ';
+        break;
+    case '=':
+      result();
+        break;
+    case 'Enter':
+      result();
+        break;
+    case 'Delete':
+      clean();
+        break;
+    case 'Escape':
+      clean();
+        break;
+    case 'Backspace':
+      cleanLast();
+        break;
+  }
+}
+
 // добавить слушатели:
 plusButton.addEventListener('click', plus);
 minusButton.addEventListener('click', minus);
@@ -101,6 +178,7 @@ divideButton.addEventListener('click', divide);
 equalButton.addEventListener('click', result);
 cleanLastButton.addEventListener('click', cleanLast);
 cleanButton.addEventListener('click', clean);
+// слушатели на кнопки
 Button1.addEventListener('click', addInteger1);
 Button2.addEventListener('click', addInteger2);
 Button3.addEventListener('click', addInteger3);
@@ -110,4 +188,7 @@ Button6.addEventListener('click', addInteger6);
 Button7.addEventListener('click', addInteger7);
 Button8.addEventListener('click', addInteger8);
 Button9.addEventListener('click', addInteger9);
-// тесты
+Button0.addEventListener('click', addInteger0);
+ButtonPoint.addEventListener('click', addPoint);
+// слушатели на клавиши
+document.addEventListener('keydown', PressKey);
